1c051b8facb9c39b832d21ed6e400506f5271016
kracekumar <me@kracekumar.com>
2025-03-19 00:17:45 +0000

    Add support to generate llms-full.txt

--- hugo2llmtxt.py
+++ hugo2llmtxt.py
@@ -1,23 +1,29 @@
 
 import typer
+import sys
 import csv
 from collections import defaultdict
 app = typer.Typer()
 
-def output(sections: list[str], output_path: str):
+def output(sections: dict[str, list[dict[str, str]]], output_path: str, render_content: bool=False):
     with open(output_path, mode='w') as f:
         count = 0
-        for section, titles in sections.items():
+        for section, articles in sections.items():
             f.write(f"# {section}\n")
-            for title in titles:
-                f.write(f"- {title}\n")
+            for article in articles:
+                f.write(f"- {article['title']}\n")
+                if render_content:
+                    f.write(article['content'])
+                    f.write('\n')
                 count += 1
         typer.echo(f"Output written to {output_path}. Total links: {count}")
 
 @app.command()
 def llm2txt(file_path: str = typer.Argument(..., help="Path to the CSV file obtained by running `hugo list all > hugo_list.all.csv`"),
-            ouput_path: str = typer.Argument(..., help="Path to the output text file like `static/llms.txt`")):
-    sections = defaultdict(list)
+            ouput_path: str = typer.Argument(..., help="Path to the output text file like `static/llms.txt`"),
+            render_content: bool = typer.Argument(..., help="Whether to output entire blog content to a file. Pass true for `llms-full.txt`")):
+    sections = defaultdict(list[dict[str, str]])
+    # sections = {'page': [{'title': 'hello world', 'content': 'This is hollow world'}]}
     try:
         with open(file_path, mode='r', newline='') as csvfile:
             reader = csv.DictReader(csvfile)
@@ -27,13 +33,19 @@ def llm2txt(file_path: str = typer.Argument(..., help="Path to the CSV file obta
                 # If the section is empty string not None
                 section = row.get('section', 'Misc') or 'Misc'
                 if  title:
-                    sections[section].append(f"[{title}]({permalink}): {title}")
-            output(sections, ouput_path)
+                    item = {}
+                    item['title'] = f"[{title}]({permalink}index.md): {title}"
+                    try:
+                        item['content'] = open(row['path']).read()
+                    except FileNotFoundError:
+                        typer.echo(f"File not found: {row['path']}", err=True)
+                        sys.exit(-1)
+                    sections[section].append(item)
+            output(sections, ouput_path, render_content)
     except FileNotFoundError:
-        typer.echo(f"File not found: {file_path}")
+        typer.echo(f"File not found: {file_path}", err=True)
     except Exception as e:
         typer.echo(f"An error occurred: {e}")
-
 
 if __name__ == "__main__":
     app()
